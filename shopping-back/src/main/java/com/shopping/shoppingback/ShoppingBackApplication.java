5a45079e9a3ca7d98abf9868238e624a42541e88
siggu <m0_w_0m@naver.com>
2023-10-10 16:00:16 +0900

    Folder Structure

---
+++ shopping-back/src/main/java/com/shopping/shoppingback/ShoppingBackApplication.java
@@ -0,0 +1,12 @@
+package com.shopping.shoppingback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingBackApplication {
+  
+  public static void main(String[] args) {
+    SpringApplication.run(ShoppingBackApplication.class, args);
+  }
+}
