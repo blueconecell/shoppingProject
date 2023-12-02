c2629f528c4e5e3e916b36da4be21ad592ef6228
siggu <m0_w_0m@naver.com>
2023-12-02 19:56:19 +0900

    mock 변경

--- shoping-front/src/mocks/category-product-list-bathroom-test.mock.ts
+++ shoping-front/src/mocks/category-product-list-bathroom-test.mock.ts
@@ -36,26 +36,30 @@ const categoryProductListMock_bathroomTest: ProductInfo[] = [
     productId: 0,
     categoryId: 0,
     categoryName: "욕실",
-    categoryImage: "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/166848856331604255.jpg?w=1080",
+    categoryImage:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/166848856331604255.jpg?w=1080",
     title: "코멧 순백 3겹 라벤더 바닐라 롤화장지 30m 30롤, 1팩",
     price: 13900,
     productImage:
       "https://github.com/blueconecell/shoppingProject/blob/main/shoping-front/src/assets/image/toilet-paper.jpg?raw=true",
-    productInfo: "https://www.coupang.com/vp/products/261192458?itemId=816702952&vendorItemId=5077941266&src=1032034&spec=10305197&addtag=400&ctag=261192458&lptag=I816702952&itime=20231202150225&pageType=PRODUCT&pageValue=261192458&wPcid=19800813108875055932084&wRef=prod.danawa.com&wTime=20231202150225&redirect=landing&mcid=9baa2a9abf7448d9bb8833a5bcbe14c8&isAddedCart=",
+    productInfo:
+      "https://www.coupang.com/vp/products/261192458?itemId=816702952&vendorItemId=5077941266&src=1032034&spec=10305197&addtag=400&ctag=261192458&lptag=I816702952&itime=20231202150225&pageType=PRODUCT&pageValue=261192458&wPcid=19800813108875055932084&wRef=prod.danawa.com&wTime=20231202150225&redirect=landing&mcid=9baa2a9abf7448d9bb8833a5bcbe14c8&isAddedCart=",
     tagName: "휴지",
     viewCount: 0,
-    isLike: true,
+    isLike: false,
   },
   {
     productId: 0,
     categoryId: 0,
     categoryName: "욕실",
-    categoryImage: "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/166848856331604255.jpg?w=1080",
+    categoryImage:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/166848856331604255.jpg?w=1080",
     title: "코멧 호텔 수건 150g 30수 면사",
-    price: 13900,
+    price: 26990,
     productImage:
       "https://thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/14446098276487-4f58df2e-bddb-4c81-b2da-d86d00fcf756.jpg",
-    productInfo: "https://www.coupang.com/vp/products/261192458?itemId=816702952&vendorItemId=5077941266&src=1032034&spec=10305197&addtag=400&ctag=261192458&lptag=I816702952&itime=20231202150225&pageType=PRODUCT&pageValue=261192458&wPcid=19800813108875055932084&wRef=prod.danawa.com&wTime=20231202150225&redirect=landing&mcid=9baa2a9abf7448d9bb8833a5bcbe14c8&isAddedCart=",
+    productInfo:
+      "https://www.coupang.com/vp/products/187554379?itemId=536057673&vendorItemId=4813726643&src=1042503&spec=10304991&addtag=400&ctag=187554379&lptag=10304991I536057673&itime=20231202194817&pageType=PRODUCT&pageValue=187554379&wPcid=17015140932557692363235&wRef=&wTime=20231202194817&redirect=landing&gclid=EAIaIQobChMI16fC9snwggMVD9oWBR0APwIREAQYASABEgKg8vD_BwE&mcid=5fbc2563cfae411a8691f527f4a7c299&campaignid=20522662249&adgroupid=&isAddedCart=",
     tagName: "휴지",
     viewCount: 0,
     isLike: true,
@@ -64,26 +68,30 @@ const categoryProductListMock_bathroomTest: ProductInfo[] = [
     productId: 0,
     categoryId: 0,
     categoryName: "욕실",
-    categoryImage: "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/166848856331604255.jpg?w=1080",
-    title: "코멧 순백 3겹 라벤더 바닐라 롤화장지 30m 30롤, 1팩",
-    price: 13900,
+    categoryImage:
+      "https://thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1656206318725660-45ae0008-89c9-4fd0-878d-09b1e66c7a5b.jpg",
+    title: "오랄비 크로스액션 컴플리트7 35모 칫솔 6개입, 1개",
+    price: 12780,
     productImage:
-      "https://github.com/blueconecell/shoppingProject/blob/main/shoping-front/src/assets/image/toilet-paper.jpg?raw=true",
-    productInfo: "https://www.coupang.com/vp/products/261192458?itemId=816702952&vendorItemId=5077941266&src=1032034&spec=10305197&addtag=400&ctag=261192458&lptag=I816702952&itime=20231202150225&pageType=PRODUCT&pageValue=261192458&wPcid=19800813108875055932084&wRef=prod.danawa.com&wTime=20231202150225&redirect=landing&mcid=9baa2a9abf7448d9bb8833a5bcbe14c8&isAddedCart=",
+      "https://thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1656206318725660-45ae0008-89c9-4fd0-878d-09b1e66c7a5b.jpg",
+    productInfo:
+      "https://www.coupang.com/vp/products/4930608850?itemId=6478112626&vendorItemId=73772402530&sourceType=srp_product_ads&clickEventId=6860b5a0-9100-11ee-a905-f0f1cf067c1c&korePlacement=15&koreSubPlacement=1&q=%EC%98%A4%EB%9E%84%EB%B9%84+%ED%81%AC%EB%A1%9C%EC%8A%A4%EC%95%A1%EC%85%98+%EC%BB%B4%ED%94%8C%EB%A6%AC%ED%8A%B87+35%EB%AA%A8+%EC%B9%AB%EC%86%94+6%EA%B0%9C%EC%9E%85%2C+1%EA%B0%9C+12780+%EC%9B%90&itemsCount=36&searchId=0f56e862a45e4a779d0a550d8d330248&rank=0&isAddedCart=",
     tagName: "휴지",
     viewCount: 0,
-    isLike: true,
+    isLike: false,
   },
   {
     productId: 0,
     categoryId: 0,
     categoryName: "욕실",
-    categoryImage: "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/166848856331604255.jpg?w=1080",
-    title: "코멧 순백 3겹 라벤더 바닐라 롤화장지 30m 30롤, 1팩",
-    price: 13900,
+    categoryImage:
+      "https://image.ohou.se/i/bucketplace-v2-development/uploads/cards/projects/166848856331604255.jpg?w=1080",
+    title: "코멧 홈 파워수압 필터 샤워기 본품 + 필터 2개입",
+    price: 7900,
     productImage:
-      "https://github.com/blueconecell/shoppingProject/blob/main/shoping-front/src/assets/image/toilet-paper.jpg?raw=true",
-    productInfo: "https://www.coupang.com/vp/products/261192458?itemId=816702952&vendorItemId=5077941266&src=1032034&spec=10305197&addtag=400&ctag=261192458&lptag=I816702952&itime=20231202150225&pageType=PRODUCT&pageValue=261192458&wPcid=19800813108875055932084&wRef=prod.danawa.com&wTime=20231202150225&redirect=landing&mcid=9baa2a9abf7448d9bb8833a5bcbe14c8&isAddedCart=",
+      "https://thumbnail7.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2503748766576084-08b80611-aea6-447f-8691-13170a564fa7.jpg",
+    productInfo:
+      "https://www.coupang.com/vp/products/7178787283?itemId=18101284961&vendorItemId=85253394509&pickType=COU_PICK&q=%22%EC%BD%94%EB%A9%A7+%ED%99%88+%ED%8C%8C%EC%9B%8C%EC%88%98%EC%95%95+%ED%95%84%ED%84%B0+%EC%83%A4%EC%9B%8C%EA%B8%B0+%EB%B3%B8%ED%92%88+++%ED%95%84%ED%84%B0+2%EA%B0%9C%EC%9E%85%22%2C&itemsCount=36&searchId=b07abf98eb6a462eb3f2c4024c72453e&rank=2&isAddedCart=",
     tagName: "휴지",
     viewCount: 0,
     isLike: true,
--- shoping-front/src/mocks/category-product-list-kitchen-test.mock.ts
+++ shoping-front/src/mocks/category-product-list-kitchen-test.mock.ts
@@ -35,12 +35,30 @@ const categoryProductListMock_kitchenTest: ProductInfo[] = [
     productId: 0,
     categoryId: 0,
     categoryName: "주방",
-    categoryImage: "https://img.freepik.com/premium-photo/vertical-photo-of-the-corridor-of-a-new-kitchen_385881-1225.jpg",
+    categoryImage:
+      "https://img.freepik.com/premium-photo/vertical-photo-of-the-corridor-of-a-new-kitchen_385881-1225.jpg",
     title: "쓰리엠 베이직 제로 스크레치 스펀지 수세미",
     price: 8990,
     productImage:
       "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1925889153491061-d774dd39-3634-435a-b7e8-2ecb83eee270.jpg",
-    productInfo: "https://www.coupang.com/vp/products/2140167813?itemId=3634663228&vendorItemId=71620215807&pickType=COU_PICK&q=%EC%93%B0%EB%A6%AC%EC%97%A0+%EB%B2%A0%EC%9D%B4%EC%A7%81+%EC%A0%9C%EB%A1%9C+%EC%8A%A4%ED%81%AC%EB%A0%88%EC%B9%98+%EC%8A%A4%ED%8E%80%EC%A7%80+%EC%88%98%EC%84%B8%EB%AF%B8&itemsCount=36&searchId=0c436a3ba9334881ad7716c5ee0a20f9&rank=1&isAddedCart=",
+    productInfo:
+      "0https://www.coupang.com/vp/products/2140167813?itemId=3634663228&vendorItemId=71620215807&q=%EC%93%B0%EB%A6%AC%EC%97%A0+%EB%B2%A0%EC%9D%B4%EC%A7%81+%EC%A0%9C%EB%A1%9C+%EC%8A%A4%ED%81%AC%EB%A0%88%EC%B9%98+%EC%8A%A4%ED%8E%80%EC%A7%80+%EC%88%98%EC%84%B8%EB%AF%B8&itemsCount=36&searchId=186c14dd31b646198280fa5aab5d1e3f&rank=1&isAddedCart=",
+    tagName: "휴지",
+    viewCount: 0,
+    isLike: false,
+  },
+  {
+    productId: 0,
+    categoryId: 0,
+    categoryName: "주방",
+    categoryImage:
+      "https://img.freepik.com/premium-photo/vertical-photo-of-the-corridor-of-a-new-kitchen_385881-1225.jpg",
+    title: "마미손 플라워 고무장갑 M",
+    price: 8990,
+    productImage:
+      "https://thumbnail9.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/9006707781139898-15e6f114-e5b1-4af9-92d9-ebdcd9f0ae6f.jpg",
+    productInfo:
+      "https://www.coupang.com/vp/products/5853927999?itemId=10195050859&vendorItemId=70361791831&q=%EB%A7%88%EB%AF%B8%EC%86%90+%ED%94%8C%EB%9D%BC%EC%9B%8C+%EA%B3%A0%EB%AC%B4%EC%9E%A5%EA%B0%91+M+8990dnjs&itemsCount=36&searchId=4c4c78dcb42848cdb13ae193204d8de8&rank=15&isAddedCart=",
     tagName: "휴지",
     viewCount: 0,
     isLike: true,
@@ -49,12 +67,14 @@ const categoryProductListMock_kitchenTest: ProductInfo[] = [
     productId: 0,
     categoryId: 0,
     categoryName: "주방",
-    categoryImage: "https://img.freepik.com/premium-photo/vertical-photo-of-the-corridor-of-a-new-kitchen_385881-1225.jpg",
-    title: "코멧 순백 3겹 라벤더 바닐라 롤화장지 30m 30롤, 1팩",
+    categoryImage:
+      "https://img.freepik.com/premium-photo/vertical-photo-of-the-corridor-of-a-new-kitchen_385881-1225.jpg",
+    title: "락앤락 뉴 김치통 정석 핑크",
     price: 8990,
     productImage:
-      "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1925889153491061-d774dd39-3634-435a-b7e8-2ecb83eee270.jpg",
-    productInfo: "https://www.coupang.com/vp/products/2140167813?itemId=3634663228&vendorItemId=71620215807&pickType=COU_PICK&q=%EC%93%B0%EB%A6%AC%EC%97%A0+%EB%B2%A0%EC%9D%B4%EC%A7%81+%EC%A0%9C%EB%A1%9C+%EC%8A%A4%ED%81%AC%EB%A0%88%EC%B9%98+%EC%8A%A4%ED%8E%80%EC%A7%80+%EC%88%98%EC%84%B8%EB%AF%B8&itemsCount=36&searchId=0c436a3ba9334881ad7716c5ee0a20f9&rank=1&isAddedCart=",
+      "https://thumbnail8.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/6435985670344590-6bdbccd3-4659-4cac-b781-eebddbd54096.jpg",
+    productInfo:
+      "https://www.coupang.com/vp/products/6112950426?itemId=11533341579&vendorItemId=78808412929&q=%EB%9D%BD%EC%95%A4%EB%9D%BD+%EB%89%B4+%EA%B9%80%EC%B9%98%ED%86%B5+%EC%A0%95%EC%84%9D+%ED%95%91%ED%81%AC&itemsCount=36&searchId=0cb883d70b3243a9b8ff79c7caf400ad&rank=7&isAddedCart=",
     tagName: "휴지",
     viewCount: 0,
     isLike: true,
@@ -63,29 +83,17 @@ const categoryProductListMock_kitchenTest: ProductInfo[] = [
     productId: 0,
     categoryId: 0,
     categoryName: "주방",
-    categoryImage: "https://img.freepik.com/premium-photo/vertical-photo-of-the-corridor-of-a-new-kitchen_385881-1225.jpg",
-    title: "코멧 순백 3겹 라벤더 바닐라 롤화장지 30m 30롤, 1팩",
+    categoryImage:
+      "https://img.freepik.com/premium-photo/vertical-photo-of-the-corridor-of-a-new-kitchen_385881-1225.jpg",
+    title: "프릴 베이킹소다 주방세제 퓨어 레몬 700ml, 1개",
     price: 8990,
     productImage:
-      "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1925889153491061-d774dd39-3634-435a-b7e8-2ecb83eee270.jpg",
-    productInfo: "https://www.coupang.com/vp/products/2140167813?itemId=3634663228&vendorItemId=71620215807&pickType=COU_PICK&q=%EC%93%B0%EB%A6%AC%EC%97%A0+%EB%B2%A0%EC%9D%B4%EC%A7%81+%EC%A0%9C%EB%A1%9C+%EC%8A%A4%ED%81%AC%EB%A0%88%EC%B9%98+%EC%8A%A4%ED%8E%80%EC%A7%80+%EC%88%98%EC%84%B8%EB%AF%B8&itemsCount=36&searchId=0c436a3ba9334881ad7716c5ee0a20f9&rank=1&isAddedCart=",
+      "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/2023/03/13/15/7/bbe53942-5e45-4fd9-8377-97319c8964ba.jpg",
+    productInfo:
+      "https://www.coupang.com/vp/products/7235465113?itemId=18626586195&vendorItemId=85761961954&q=%ED%94%84%EB%A6%B4+%EB%B2%A0%EC%9D%B4%ED%82%B9%EC%86%8C%EB%8B%A4+%EC%A3%BC%EB%B0%A9%EC%84%B8%EC%A0%9C+%ED%93%A8%EC%96%B4+%EB%A0%88%EB%AA%AC+700ml%2C+1%EA%B0%9C&itemsCount=36&searchId=d187e38d91f14e858e8d96878abd8391&rank=6&isAddedCart=",
     tagName: "휴지",
     viewCount: 0,
-    isLike: true,
-  },
-  {
-    productId: 0,
-    categoryId: 0,
-    categoryName: "주방",
-    categoryImage: "https://img.freepik.com/premium-photo/vertical-photo-of-the-corridor-of-a-new-kitchen_385881-1225.jpg",
-    title: "코멧 순백 3겹 라벤더 바닐라 롤화장지 30m 30롤, 1팩",
-    price: 8990,
-    productImage:
-      "https://thumbnail6.coupangcdn.com/thumbnails/remote/230x230ex/image/retail/images/1925889153491061-d774dd39-3634-435a-b7e8-2ecb83eee270.jpg",
-    productInfo: "https://www.coupang.com/vp/products/2140167813?itemId=3634663228&vendorItemId=71620215807&pickType=COU_PICK&q=%EC%93%B0%EB%A6%AC%EC%97%A0+%EB%B2%A0%EC%9D%B4%EC%A7%81+%EC%A0%9C%EB%A1%9C+%EC%8A%A4%ED%81%AC%EB%A0%88%EC%B9%98+%EC%8A%A4%ED%8E%80%EC%A7%80+%EC%88%98%EC%84%B8%EB%AF%B8&itemsCount=36&searchId=0c436a3ba9334881ad7716c5ee0a20f9&rank=1&isAddedCart=",
-    tagName: "휴지",
-    viewCount: 0,
-    isLike: true,
+    isLike: false,
   },
 ];
 
