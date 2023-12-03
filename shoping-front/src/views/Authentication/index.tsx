c8d6244a4fd3d4fa832c1f9ec868bc5a11c5aef8
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-12-03 14:46:04 +0900

    유저화면 업데이트

--- shoping-front/src/views/Authentication/index.tsx
+++ shoping-front/src/views/Authentication/index.tsx
@@ -471,15 +471,7 @@ export default function Authentication() {
   return (
     <div id="auth-wrapper">
       <div className="auth-container">
-        {/* <div className='auth-jumbotron-box'>
-                    <div className='auth-jumbotron-contents'>
-                        <div className='auth-logo-icon'></div>
-                        <div className='auth-jumbotron-text-box'>
-                            <div className='auth-jumbotron-text'>{'환영합니다'}</div>
-                            <div className='auth-jumbotron-text'>{'HOONS BOARD 입니다.'}</div>
-                        </div>
-                    </div>
-                </div> */}
+
         {view === "sign-in" && <SignInCard />}
         {view === "sign-up" && <SignUpCard />}
       </div>
--- shoping-front/src/views/User/index.tsx
+++ shoping-front/src/views/User/index.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import './style.css'
+import useLoginUserStore from 'stores/login-user.store';
 
 // component: 유저 화면 컴포넌트
 export default function User() {
+  const { loginUser } = useLoginUserStore();
 
   // render: 유저 화면 컴포넌트 렌더링
   return (
-    <div>유저 화면</div>
+    <div className='user-card'>
+      <div className='user-card-nickname'>"{loginUser?.nickname}" 님 반갑습니다!</div>
+      <div className='user-card-email'>이메일 : {loginUser?.email}</div>
+    </div>
+
   )
 }
