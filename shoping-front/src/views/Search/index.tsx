f758e47c707522700bce109c3f11983bd8164768
siggu <m0_w_0m@naver.com>
2023-12-03 14:48:46 +0900

    gitignore

--- shoping-front/src/views/Search/index.tsx
+++ shoping-front/src/views/Search/index.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
+import { useParams } from "react-router-dom";
 
 // component: 검색 화면 컴포넌트
 export default function Search() {
-
   // render: 검색 화면 컴포넌트 렌더링
-  return (
-    <div>검색 화면</div>
-  )
+  <div className=""></div>;
+  return <div className="asdf">검색 화면</div>;
 }
