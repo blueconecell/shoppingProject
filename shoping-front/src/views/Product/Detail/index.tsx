6ca686356b5a5e19f511320f53a9e2af5ceca50c
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-12-03 15:23:44 +0900

    검색창 성공

--- shoping-front/src/views/Product/Detail/index.tsx
+++ shoping-front/src/views/Product/Detail/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import './style.css'
+import ProductItem from 'components/ProductItem/test'
+import { products } from 'mocks'
 
 // component: 게시물 상세 화면 컴포넌트
 export default function ProductDetail() {
 
   // render: 게시물 상세 화면 컴포넌트 렌더링
   return (
-    <div>게시물 상세 화면</div>
+    <div className='product'>
+      <div className="top-3-product-item">
+        {products.map((productList) => (
+        <ProductItem productListItem={productList} />
+        ))}
+      </div>
+    </div>
   )
 }
--- shoping-front/src/views/Search/index.tsx
+++ shoping-front/src/views/Search/index.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import './style.css'
+import { useParams } from 'react-router-dom';
 
 // component: 검색 화면 컴포넌트
 export default function Search() {
+  const { searchWord } = useParams();
+  console.log(searchWord)
 
   // render: 검색 화면 컴포넌트 렌더링
   return (
-    <div>검색 화면</div>
+    <div>
+      <h2>Search Results for: {searchWord}</h2>
+    </div>
   )
 }
